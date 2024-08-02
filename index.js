12ca77540d6f0d00cf960ed7c2b46aa8eb91a12b
janbolot-web <janbolotcode@gmail.com>
2024-08-02 12:52:26 +0600

    change

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
-
 app.use("/api", router);
 
 
